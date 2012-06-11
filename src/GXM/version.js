2845504051a6796fb29d982bb6e3fa45832d3790
Marc Jansen <jansen@terrestris.de>
2012-06-11 16:31:11 +0200

    Give the GXM namespace a member version to identify itself.
    Add a version.js-file that functions as our namespace declaration. The only
    purpose of this file is to setup the namespace 'GXM' and give it a member
    'version' which identifies the version of GXM.

---
+++ src/GXM/version.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2012 The Open Source Geospatial Foundation
+ * 
+ * Published under the BSD license. 
+ * 
+ * See https://raw.github.com/geoext/GXM/master/license.txt for the full 
+ * text of the license.
+ */
+/**
+ * The GXM root object
+ * @class GXM
+ * @singleton
+ */
+Ext.define('GXM.version', {
+    singleton: true
+});
+GXM.version = 'pre-0.2';
