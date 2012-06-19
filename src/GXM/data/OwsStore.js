7693425f92848cf34c309570ad6f1fba9b49ec50
Matt Priour <mpriour@opengeo.org>
2012-06-18 19:57:17 -0500

    Missed dependency of AttributeStore

---
+++ src/GXM/data/OwsStore.js
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2008-2012 The Open Source Geospatial Foundation
+ *
+ * Published under the BSD license.
+ * See https://github.com/geoext/geoext2/blob/master/license.txt for the full text
+ * of the license.
+ */
+
+/**
+ * <p>Small Base class to make creating stores for remote OWS information sources
+ * easier.</p>
+ * <p>NOTE: This is a BASE CLASS and is not designed for direct use in an
+ * application. Instead, one should extend from this class in any situation in
+ * which a you need a {@link Ext.data.proxy.Server} (ex: 'ajax', 'jsonp', etc) and a
+ * reader which requires an {@link OpenLayers.Format} to parse the data.</p>
+ *
+ * @class GeoExt.data.OwsStore
+ */
+Ext.define('GXM.data.OwsStore', {
+    extend: 'Ext.data.Store',
+    alternateClassName: ['GXM.data.OWSStore'],
+
+    config: {
+        /**
+         * @cfg {String}
+         * The URL from which to retrieve the WMS DescribeLayer document
+         */
+        url : null,
+
+        /**
+         * @cfg {OpenLayers.Format}
+         * A parser for transforming the XHR response into an array of objects
+         * representing attributes. Defaults to an {OpenLayers.Format.WMSDescribeLayer}
+         * parser.
+         */
+        format : null
+    },
+
+    /**
+     * @private
+     */
+    constructor: function(config) {
+        // At this point, we have to copy the complex objects from the config
+        // into the prototype. This is because Ext.data.Store's constructor 
+        // creates deep copies of these objects.
+        var format;
+        if (config.format) {
+            format = config.format;
+            delete config.format;
+        }
+        
+        this.callParent([config]);
+
+        if(format) {
+            this.setFormat(format);
+        }
+    },
+    
+    /**
+     * @private
+     */
+    applyUrl: function(newValue) {
+        if(newValue && Ext.isString(newValue)) {
+            var proxy = this.getProxy();
+            if(proxy && proxy.setUrl) {
+                proxy.setUrl(newValue);
+            }
+        }
+    },
+    
+    /**
+     * @private
+     */
+    applyFormat: function(newFormat) {
+        var proxy = this.getProxy();
+        var reader = (proxy) ? proxy.getReader() : null;
+        if(reader) {
+            reader.setFormat(newFormat);
+        }
+    }
+});
