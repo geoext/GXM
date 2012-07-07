753c05a1a65f35a94bc8c4a61d23cb7fcbd0f813
Cédric Moullet <cedricmoullet@localhost.(none)>
2012-07-07 08:43:18 +0200

    JSLinitification of main classes

--- src/GXM/FeaturePopup.js
+++ src/GXM/FeaturePopup.js
@@ -1,3 +1,4 @@
+/*global Ext: true, OpenLayers: true, GXM: true */
 /*
  * Copyright (c) 2012 The Open Source Geospatial Foundation
  * 
@@ -10,22 +11,22 @@
 /**
  * @class GXM.FeaturePopup
  * The class that is used to construct a GXM FeaturePopup.
- * 
+ *
  * Sample code create a lazily instanciated GXM.FeaturePopup:
- * 
+ *
  *      var featPopup = {
  *          xtype: 'gxm_featurepopup',
  *          // call with OL feature
  *          feature : olFeature
  *      };
- *      
+ *
  * Sample code create a directly instanciated GXM.FeaturePopup:
- * 
+ *
  *      var featPopup = Ext.create('GXM.FeaturePopup', {
  *          feature : olFeature,
  *          renderTo: 'popup'
  *      });
- *     
+ *
  */
 Ext.define('GXM.FeaturePopup', {
 
@@ -33,77 +34,77 @@ Ext.define('GXM.FeaturePopup', {
     requires: [
         'GXM.version'
     ],
-    xtype : 'gxm_featurepopup',
-    
+    xtype: 'gxm_featurepopup',
+
     /**
      * @property {Boolean} scrollable
      * @private
      *  A FeaturePopup is always scrollable.
      */
-	scrollable: true,
-	
+    scrollable: true,
+
     /**
      * @property {String} layout
      * @private
      *  Currently all FeaturePopups are in the fit-layout.
      */
-	layout: 'fit',
-	
+    layout: 'fit',
+
     config: {
-    	
+
         /**
          * @cfg {String} cls
          * The CSS class used to customize this popup.
          */
-    	cls: 'gxm-feature-popup',
-    	
+        cls: 'gxm-feature-popup',
+
         /**
          * @cfg {Boolean} hideOnMaskTap
-         * shall the popup hide when the surrounding mask is being 
+         * shall the popup hide when the surrounding mask is being
          *  tapped? Also see `the original documentation <http://docs.sencha.com/touch/2-0/#!/api/Ext.Panel-cfg-hideOnMaskTap>`_
          */
-    	hideOnMaskTap: true,
-    	
+        hideOnMaskTap: true,
+
         /**
          * @cfg {OpenLayers.Feature.Vector} feature
-         * <http://dev.openlayers.org/releases/OpenLayers-2.11/doc/apidocs/files/OpenLayers/Feature/Vector-js.html>`_ 
+         * <http://dev.openlayers.org/releases/OpenLayers-2.11/doc/apidocs/files/OpenLayers/Feature/Vector-js.html>`_
          *  The feature this popup is about to derive its information from.
          */
         feature: null,
-        
+
         /**
          * @cfg {Ext.XTemplate <http://docs.sencha.com/touch/2-0/#!/api/Ext.XTemplate>} tpl
-         * The template used to render the popup content. If not provided, a 
-         *  rudimentary template is being used. 
+         * The template used to render the popup content. If not provided, a
+         *  rudimentary template is being used.
          */
         tpl: null,
-        
+
         /**
          * @cfg {Boolean} styleHtmlContent
-         *  shall HTML content be styled when rendering content? 
+         *  shall HTML content be styled when rendering content?
          *   Also see `the original documentation <http://docs.sencha.com/touch/2-0/#!/api/Ext.Panel-cfg-styleHtmlContent>`_
          */
         styleHtmlContent: true,
-        
+
         /**
          * @cfg {Boolean} centered
-         * shall the popup be centered within the viewport? 
+         * shall the popup be centered within the viewport?
          *  Also see `the original documentation <http://docs.sencha.com/touch/2-0/#!/api/Ext.Panel-cfg-centered>`_
          */
-        centered : true,
-        
+        centered: true,
+
         /**
          * @cfg {Boolean} modal
-         *  Should the panel be modal? 
+         *  Should the panel be modal?
          */
-        modal : true
+        modal: true
     },
-    
+
     /**
      * @private
      * The constructor function
      */
-    constructor: function(config){
+    constructor: function (config) {
         if (!config.tpl) {
             config.tpl = new Ext.XTemplate('<p>You clicked the feature with the ID: {feature.id}</p>', '<p> Overwrite the config-property tpl to get a custom text.</p>');
         }
@@ -113,55 +114,53 @@ Ext.define('GXM.FeaturePopup', {
         }
         this.callParent(arguments);
     },
-    
+
     /**
      * @private
      * Initializes the Component.
      */
-    initialize: function() {
-    	
-    	var feature = this.getFeature();
-    	
-    	// set the given feature in order
-    	// to call the apply method
-    	// which cares about the data types
-    	// necessary because we do the manual setting
-    	// of feature in constructor due to 
-    	// known issues with circular
-    	// references
-    	if (Ext.isDefined(feature)) {
-    		this.setFeature(feature);
-    	}
-    	
+    initialize: function () {
+
+        var feature = this.getFeature();
+
+        // set the given feature in order
+        // to call the apply method
+        // which cares about the data types
+        // necessary because we do the manual setting
+        // of feature in constructor due to
+        // known issues with circular
+        // references
+        if (Ext.isDefined(feature)) {
+            this.setFeature(feature);
+        }
+
         this.callParent();
     },
-    
+
     /**
      * @private
      * Intersects the :func:`setFeature` function.
-     *  Adds the passed feature via setData to prevent data loss. Is needed to 
+     *  Adds the passed feature via setData to prevent data loss. Is needed to
      *  support more than one datatype for the new feature.
-     *  
+     *
      *  @param {Object} newFeature the new feature representation.
      *  @return {OpenLayers.Feature}
      */
-    applyFeature: function(newFeature) {
-		if(newFeature instanceof OpenLayers.Feature.Vector) {
-			
-			// set the feature as data, because if we pass
-			// an OL object directly as data it gets lost
-			this.setData({
-				feature: newFeature
-			});
-		}
-		else if (feature instanceof OpenLayers.Geometry) {
-			// TODO
-		}
-		else if (feature instanceof Ext.Record) {
-			// TODO
-		}
-		
-		return newFeature;
-	}
-    
+    applyFeature: function (newFeature) {
+        if (newFeature instanceof OpenLayers.Feature.Vector) {
+
+            // set the feature as data, because if we pass
+            // an OL object directly as data it gets lost
+            this.setData({
+                feature: newFeature
+            });
+        } else if (feature instanceof OpenLayers.Geometry) {
+            // TODO
+        } else if (feature instanceof Ext.Record) {
+            // TODO
+        }
+
+        return newFeature;
+    }
+
 });
